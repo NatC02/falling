76cb85829d85afdbe083b723994e06f09d6c706f
NatC02 <natanceballos02@gmail.com>
2025-03-31 03:31:23 -0400

    initial imports for Three.js scene components

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import { AudioManager } from './audioManager';
+import { circuitShader } from './circuitShader';
